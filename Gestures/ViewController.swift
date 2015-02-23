0cff28fea22d7a48d9013679504763a40a680087
Matthew Nielsen <mqnielsen@me.com>
2015-02-22 22:35:44 -0500

    Placed constraints on moving the rectangle outside of the screen.

--- Gestures/ViewController.swift
+++ Gestures/ViewController.swift
@@ -6,6 +6,10 @@
 //  Copyright (c) 2015 Matthew Nielsen. All rights reserved.
 //
 
+// This tutorial was very helpful: http://www.raywenderlich.com/76020/using-uigesturerecognizer-with-swift-tutorial
+// NOTE: swipe gesture not possible in combination with pan gesture
+// NOTE: placing constraints on the rectangle while panning could get it stuck if the user resizes it
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -28,19 +32,30 @@ class ViewController: UIViewController {
         currentColor = 0
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
     func incrementCurrentColor() {
         currentColor! = (currentColor! + 1) % (colors.count - 1)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func rectangleOnScreen(rectangle: UIView) -> Bool {
+        var screenWidth = UIScreen.mainScreen().bounds.size.width
+        var screenHeight = UIScreen.mainScreen().bounds.size.height
+        
+        if rectangle.frame.maxX > screenWidth || rectangle.frame.maxY > screenHeight || rectangle.frame.minX < 0 || rectangle.frame.minY < 0 {
+            return false
+        } else {
+            return true
+        }
     }
     
     @IBAction func longPress(recognizer: UILongPressGestureRecognizer) {
         println("Long Press Gesture")
         
         if recognizer.state == .Ended {
-            recognizer.view!.transform = CGAffineTransformRotate(recognizer.view!.transform, 1)
+            recognizer.view!.transform = CGAffineTransformRotate(recognizer.view!.transform, CGFloat(M_PI / 2))
         }
     }
     
@@ -68,10 +83,20 @@ class ViewController: UIViewController {
     @IBAction func pan(recognizer: UIPanGestureRecognizer) {
         println("Pan (Drag) Gesture")
         
+        // get the center point of the rectangle before moving it
+        let oldCenter = recognizer.view!.center
+        
+        // move the rectangle
         let translation = recognizer.translationInView(self.view)
         recognizer.view!.center = CGPoint(x: recognizer.view!.center.x + translation.x, y: recognizer.view!.center.y + translation.y)
+        
+        // don't make the translation if the new center puts it outside of the screen
+        if !rectangleOnScreen(recognizer.view!) {
+            recognizer.view!.center = oldCenter
+        }
+        
+        // set the translation
         recognizer.setTranslation(CGPointZero, inView: self.view)
     }
     
 }
-
